6b4015f9db9aa2140857a33ce201dedc7b0bcc9f
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-02-27 23:13:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import './about.css';
 import PasswordGenerator from './About';
 import Cards from './Component/Profilecards'
 import { useState } from 'react';
-import TestimonialSlider from './img/Slider';
 
 function App() {
 
   return (
     <div style={{display:"flex",flexWrap:"wrap",padding:"10px"}}>
       <PasswordGenerator />
-      <TestimonialSlider />
     </div>
   );
 }
